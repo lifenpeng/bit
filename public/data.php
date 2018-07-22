484de0c3f59dc31f502e0dca1316b819f9c50405
lifenpeng <1170275451@qq.com>
2018-07-22 23:07:19 +0800

    数据库连接成功 并把数据转化为json格式

--- public/data.php
+++ public/data.php
@@ -1,17 +1,51 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
-error_reporting(0); 
-function studentData($msg){
-    if($msg==1){
-         $arr = array('name'=>'junjun','age'=>'18','sex'=>'女');
-         return json_encode($arr);
+
+$article = array();
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "blog";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+
+$conn->query("SET NAMES utf8");
+ 
+$sql = "SELECT * FROM my_adarticle";
+
+$result = $conn->query($sql);
+ 
+if ($result->num_rows > 0) {
+    // 输出数据
+    while($row = $result->fetch_assoc()) {
+        array_push($article,$row);
     }
+} else {
+    echo "0 结果";
 }
 
-if($_GET['msg']){
-    echo studentData($_GET['msg']);
-}else{
-    echo 'false';
+$arr = array();
+
+foreach($article as $v){
+   array_push($arr,$v);
 }
+
+if($_GET['msg']==1){
+    $datas = ['data'=>$arr];
+    echo json_encode($datas);
+}else{
+    $datas = ['data'=>'请求参数错误'];
+    echo json_encode($datas);
+}
+
+$conn->close();
+
+
 
