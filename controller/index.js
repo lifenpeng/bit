bdbf783094d96df48e85fd4207c7a093c905978c
lifenpeng <1170275451@qq.com>
2018-07-22 23:16:26 +0800

    通过json 接收到 data.php 数据

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,14 @@
 var app = angular.module('lfp-app',[]);
 app.controller('myApp',['$scope','$http',function($scope,$http){
-
+    $http({
+        method:'POST',
+        url:'http://localhost/public/data.php?msg=1'
+    }).success(function(data,status,headers,config){
+        console.log('success');
+       // console.log(data.data);
+        $scope.arr = data.data;
+        console.log($scope.arr);
+    }).error(function(data,status,headers,config){
+        console.log('err');
+    })
 }])
